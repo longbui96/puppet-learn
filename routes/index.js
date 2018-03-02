7a06b714cb4fc758645cf0d6424aae76706e1670
Long Bùi <long.kyanon@gmail.com>
2018-03-03 00:07:50 +0700

    Install init's libs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+  // Buồn ngủ quá, để tạm đây vậy
 });
 
 module.exports = router;
